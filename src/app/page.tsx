181770d43004feba7af3230d2965aa886b952eaf
samuel <adeyemis710@gmail.com>
2025-02-19 16:57:38 +0100

    testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import GirlsFirst from "@/components/GirlsFirst";
 import Hero from "@/components/Hero";
+import TestimonialsComponent from "@/components/Testimonials";
 
 export default function Home(){
   return (
     <main className="flex flex-col gap-10">
       <Hero />
       <GirlsFirst />
-      {/* <Testimonials /> */}
+      <TestimonialsComponent />
 
 
     </main>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,27 +1,19 @@
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
 
 export default function TestimonialsComponent(){
     return (
-        <section>
-            <div>
-                <h1>Hear what's Our Students has to Say </h1>
-            </div>
-            <div>
+        <section className="max-w-6xl mx-auto p-2 flex flex-col gap-[5em] mt-[5em] ">
+                <h1 className="text-3xl md:text-5xl  text-pink text-center font-medium">Hear From Students Who Trust us, Tarfs </h1>
+            <div className="flex flex-col md:flex-row items-center  md:justify-between gap-5 w-full">
                 <ImageCard 
-                src="https://example.com/image1.jpg" 
-                alt="Student 1" />
-
-
-                
+                src='/s1.jpg'
+                />
                 <ImageCard 
-                src="https://example.com/image6.jpg" 
-                alt="Student 6" />
+                src='/s2.JPG' 
+                />
                 <ImageCard 
-                src="https://example.com/image7.jpg" 
-                alt="Student 7" />
-                <ImageCard 
-                src="https://example.com/image8.jpg" 
-                alt="Student 8" />
+                src='/s3.jpg' 
+                />
             </div>
         </section>
     )
@@ -29,8 +21,14 @@ export default function TestimonialsComponent(){
 
 
 
-function ImageCard(){
+function ImageCard({src}:{src:string | StaticImageData}){
     return (
-        <Image src={src} alt=""  className=""/>
+        <Image 
+        src={src} 
+        alt="tarfs"  
+        width={316}
+        height={568}
+        className="rounded-[20px] w-[316px] h-[568px] object-cover"
+        />
     )
 }
